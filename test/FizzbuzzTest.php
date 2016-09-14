808f7786c8eea35b72b4a2a83f8e6de1b9f7ce30
n01272 <n01272@cybird-52862.local>
2016-09-14 18:53:12 +0900

    add test

--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -2,4 +2,12 @@
 namespace App;
 class FizzbuzzTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     */
+    public function resultNum(){
+        $fizzbuss = new Fizzbuzz();
+        $exception = 1;
+        $this->assertSame($exception, $fizzbuss->calc(1));
+    }
 }
