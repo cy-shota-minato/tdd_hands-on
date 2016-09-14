fb2cb7a3723c3009f9a3b41f95d784365e25c36a
n01272 <n01272@cybird-52862.local>
2016-09-14 19:11:54 +0900

    add data provider

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -2,10 +2,14 @@
 namespace App;
 class Fizzbuzz
 {
-    public function calc($num){
-
-        if(($num % 3) == 0){
+    public function calc($num)
+    {
+        if(($num % 3) == 0 && ($num % 5) == 0){
+            return 'FizzBuzz';
+        }else if(($num % 3) == 0){
             return 'Fizz';
+        }else if(($num % 5) == 0){
+            return 'Buzz';
         }
         return $num;
     }
--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -12,12 +12,52 @@ class FizzbuzzTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     */
+    public function dp_result3ToFizzAnd5ToBuzz(){
+        return [
+            [3,'Fizz'],
+            [6,'Fizz'],
+            [5,'Buzz'],
+            [10,'Buzz'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider dp_result3ToFizzAnd5ToBuzz
+     */
+    public function result3ToFizzAnd5ToBuzz($num,$exception){
+        $fizzbuss = new Fizzbuzz();
+        $this->assertSame($exception, $fizzbuss->calc($num));
+    }
+
+//    /**
+//     * @test
+//     */
+//    public function result3ToFizz(){
+//        $fizzbuss = new Fizzbuzz();
+//        $exception = "Fizz";
+//        $this->assertSame($exception, $fizzbuss->calc(3));
+//        $this->assertSame($exception, $fizzbuss->calc(6));
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function result5ToBuzz(){
+//        $fizzbuss = new Fizzbuzz();
+//        $exception = "Buzz";
+//        $this->assertSame($exception, $fizzbuss->calc(5));
+//        $this->assertSame($exception, $fizzbuss->calc(10));
+//    }
+
+    /**
      * @test
      */
-    public function result3ToFizz(){
+    public function result3and5ToBuzz(){
         $fizzbuss = new Fizzbuzz();
-        $exception = "Fizz";
-        $this->assertSame($exception, $fizzbuss->calc(3));
-        $this->assertSame($exception, $fizzbuss->calc(6));
+        $exception = "FizzBuzz";
+        $this->assertSame($exception, $fizzbuss->calc(15));
+        $this->assertSame($exception, $fizzbuss->calc(30));
     }
 }
